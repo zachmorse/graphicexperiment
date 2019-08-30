480683c729d990c96335deb77fbdf268fbd259ca
zmorse <zmorse@vailresorts.com>
2019-08-30 15:33:38 -0600

    make graphic

--- src/App.js
+++ src/App.js
@@ -1,24 +1,124 @@
 import React from 'react';
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className="centering">
+        <div className="graphicContainer">
+          <div className="line"></div>
+          <div className="line"></div>
+          <div className="line"></div>
+          <div className="line"></div>
+          <div className="line"></div>
+          <div className="line"></div>
+          <div className="line"></div>
+          <div className="line"></div>
+          <div className="line"></div>
+          <div className="line"></div>
+          <div className="line"></div>
+          <div className="line"></div>
+          <div className="line"></div>
+          <div className="line"></div>
+          <div className="line"></div>
+          <div className="line"></div>
+          <div className="line"></div>
+          <div className="line"></div>
+        </div>
+      </div>
+
+      <div className="centering">
+        <div className="graphicContainer2">
+          <div className="line2"></div>
+          <div className="line2"></div>
+          <div className="line2"></div>
+          <div className="line2"></div>
+          <div className="line2"></div>
+          <div className="line2"></div>
+          <div className="line2"></div>
+          <div className="line2"></div>
+          <div className="line2"></div>
+          <div className="line2"></div>
+          <div className="line2"></div>
+          <div className="line2"></div>
+          <div className="line2"></div>
+          <div className="line2"></div>
+          <div className="line2"></div>
+          <div className="line2"></div>
+          <div className="line2"></div>
+          <div className="line2"></div>
+        </div>
+      </div>
+
+      <div className="centering">
+        <div className="graphicContainer3">
+          <div className="line3"></div>
+          <div className="line3"></div>
+          <div className="line3"></div>
+          <div className="line3"></div>
+          <div className="line3"></div>
+          <div className="line3"></div>
+          <div className="line3"></div>
+          <div className="line3"></div>
+          <div className="line3"></div>
+          <div className="line3"></div>
+          <div className="line3"></div>
+          <div className="line3"></div>
+          <div className="line3"></div>
+          <div className="line3"></div>
+          <div className="line3"></div>
+          <div className="line3"></div>
+          <div className="line3"></div>
+          <div className="line3"></div>
+        </div>
+      </div>
+
+      <div className="centering">
+        <div className="graphicContainer4">
+          <div className="line4"></div>
+          <div className="line4"></div>
+          <div className="line4"></div>
+          <div className="line4"></div>
+          <div className="line4"></div>
+          <div className="line4"></div>
+          <div className="line4"></div>
+          <div className="line4"></div>
+          <div className="line4"></div>
+          <div className="line4"></div>
+          <div className="line4"></div>
+          <div className="line4"></div>
+          <div className="line4"></div>
+          <div className="line4"></div>
+          <div className="line4"></div>
+          <div className="line4"></div>
+          <div className="line4"></div>
+          <div className="line4"></div>
+        </div>
+      </div>
+
+      <div className="centering">
+        <div className="graphicContainer5">
+          <div className="line5"></div>
+          <div className="line5"></div>
+          <div className="line5"></div>
+          <div className="line5"></div>
+          <div className="line5"></div>
+          <div className="line5"></div>
+          <div className="line5"></div>
+          <div className="line5"></div>
+          <div className="line5"></div>
+          <div className="line5"></div>
+          <div className="line5"></div>
+          <div className="line5"></div>
+          <div className="line5"></div>
+          <div className="line5"></div>
+          <div className="line5"></div>
+          <div className="line5"></div>
+          <div className="line5"></div>
+          <div className="line5"></div>
+        </div>
+      </div>
     </div>
   );
 }
